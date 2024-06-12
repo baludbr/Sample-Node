a3e2d3a8d30d84df2dd1a48f88a00a3966b58fe6
balubbr <dwarampudibalajireddy@gmail.com>
2024-06-12 22:38:43 +0530

    add

--- routes/AuthenticationRoutes.js
+++ routes/AuthenticationRoutes.js
@@ -6,5 +6,5 @@ const {verifyToken,commonLimit}=require("../JWT/jwt")
 router.post("/register",empctr.register)
 router.post("/login",empctr.login)
 router.get("/all-employees",[verifyToken,commonLimit],empctr.getAll)
-
+//export
 module.exports=router
